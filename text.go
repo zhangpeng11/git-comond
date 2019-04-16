a3c42c4b3a9069d8dc2d148c7e89668a4a620b46
Bennet Zhangpeng <bennet.zhangpeng@grabtaxi.com>
2019-04-16 15:32:47 +0800

    change feature

--- text.go
+++ text.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main(){
+<<<<<<< HEAD
 	fmt.Println("Hello Bennet Zhangpeng 2!")
+=======
+	fmt.Println("Hello Bennet Zhangpeng!")
+	fmt.Println("Hello Zhang Peng!")
+>>>>>>> change feature
 }
